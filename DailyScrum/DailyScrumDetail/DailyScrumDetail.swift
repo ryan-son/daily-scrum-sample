642ad882acc02ae8ec7b758849fa68cb76d57cc4
Ryan Son <ryan.son1002@gmail.com>
2023-03-03 21:56:19 +0900

    feat: DailyScrumDetailView UI 및 Model 추가

--- DailyScrum/DailyScrumDetail/DailyScrumDetail.swift
+++ DailyScrum/DailyScrumDetail/DailyScrumDetail.swift
@@ -7,14 +7,103 @@
 
 import SwiftUI
 
+final class DailyScrumDetailModel: ObservableObject {
+  @Published var dailyScrum: DailyScrum
+
+  init(dailyScrum: DailyScrum) {
+    self.dailyScrum = dailyScrum
+  }
+}
+
 struct DailyScrumDetailView: View {
+  @ObservedObject var model: DailyScrumDetailModel
+
+  init(model: DailyScrumDetailModel) {
+    self.model = model
+  }
+
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    List {
+      Section {
+        Button {
+        } label: {
+          Label("Start Meeting", systemImage: "timer")
+            .font(.headline)
+            .foregroundColor(.accentColor)
+        }
+        HStack {
+          Label("Length", systemImage: "clock")
+          Spacer()
+          Text(self.model.dailyScrum.duration.formatted(
+            .units())
+          )
+        }
+
+        HStack {
+          Label("Theme", systemImage: "paintpalette")
+          Spacer()
+          Text(self.model.dailyScrum.theme.name)
+            .padding(4)
+            .foregroundColor(
+              self.model.dailyScrum.theme.accentColor
+            )
+            .background(self.model.dailyScrum.theme.mainColor)
+            .cornerRadius(4)
+        }
+      } header: {
+        Text("Daily Scrum Info")
+      }
+
+      if !self.model.dailyScrum.meetings.isEmpty {
+        Section {
+          ForEach(self.model.dailyScrum.meetings) { meeting in
+            Button {
+            } label: {
+              HStack {
+                Image(systemName: "calendar")
+                Text(meeting.date, style: .date)
+                Text(meeting.date, style: .time)
+              }
+            }
+          }
+          .onDelete { indices in
+          }
+        } header: {
+          Text("Past meetings")
+        }
+      }
+
+      Section {
+        ForEach(self.model.dailyScrum.attendees) { attendee in
+          Label(attendee.name, systemImage: "person")
+        }
+      } header: {
+        Text("Attendees")
+      }
+
+      Section {
+        Button("Delete") {
+        }
+        .foregroundColor(.red)
+        .frame(maxWidth: .infinity)
+      }
+    }
+    .navigationTitle(self.model.dailyScrum.title)
+    .toolbar {
+      Button("Edit") {
+      }
+    }
   }
 }
 
 struct DailyScrumDetailView_Previews: PreviewProvider {
   static var previews: some View {
-    DailyScrumDetailView()
+    NavigationStack {
+      DailyScrumDetailView(
+        model: DailyScrumDetailModel(
+          dailyScrum: .mock
+        )
+      )
+    }
   }
 }
