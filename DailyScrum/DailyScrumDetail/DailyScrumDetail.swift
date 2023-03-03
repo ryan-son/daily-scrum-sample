fccf0a6187f9a46718615f69be03faaf928a563c
Ryan Son <ryan.son1002@gmail.com>
2023-03-03 21:52:47 +0900

    feat: DailyScrumDetail 추가

---
+++ DailyScrum/DailyScrumDetail/DailyScrumDetail.swift
@@ -0,0 +1,20 @@
+//
+//  DailyScrumDetail.swift
+//  DailyScrum
+//
+//  Created by Geonhee on 2023/03/03.
+//
+
+import SwiftUI
+
+struct DailyScrumDetailView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct DailyScrumDetailView_Previews: PreviewProvider {
+  static var previews: some View {
+    DailyScrumDetailView()
+  }
+}
